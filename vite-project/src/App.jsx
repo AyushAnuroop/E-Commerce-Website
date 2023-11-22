1021f13e7d58d9b21d1a999c7300d350f9afb0f4
Ayush <ayushsharmahello@gmail.com>
2023-11-22 20:39:09 +0530

    third commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Home from './pages/home/Home';
+import Order from './pages/order/Order';
+import Cart from './pages/cart/Cart';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import NoPage from './pages/nopage/NoPage';
+import MyState from './context/data/myState';
+import Login from './pages/registration/Login';
+import Signup from './pages/registration/Signup';
+import ProductInfo from './pages/productInfo/ProductInfo';
+import AddProduct from './pages/admin/pages/AddProduct';
+import UpdateProduct from './pages/admin/pages/UpdateProduct';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>Hello
-       </div>
-    </>
+    <MyState>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/order" element={<Order/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/productinfo/:id" element={<ProductInfo />} />
+        <Route path="/addproduct" element ={<AddProduct/>}/>
+        <Route path="/updateproduct" element ={<UpdateProduct/>}/>
+        <Route path="/*" element={<NoPage/>} />
+      </Routes>
+    </Router>
+    </MyState>
   )
 }
 
---
+++ vite-project/src/pages/allproducts/AllProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function AllProducts() {
+  return (
+    <div>
+      All Product
+    </div>
+  )
+}
+
+export default AllProducts
